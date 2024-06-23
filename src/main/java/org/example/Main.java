e305304fa0fc1edaff2943368d248d4afa2e8981
rfilippov <rfilippov@skolopendra.com>
2024-06-23 16:43:40 +0300

    fix issues

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -7,10 +9,18 @@ public class Main {
 
         System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
+        List<Integer> integers = List.of(1, 2, 3, 4, 5);
+
+        for (String i = 1; i <= 5; i++) {
 
 
             System.out.println("i = " + i);
+        }
+
+        for (int i = 0; i < integers.size(); i++) {
+
+            System.out.println("integers.get(i) = " + integers.get(i));
+
         }
     }
 }
